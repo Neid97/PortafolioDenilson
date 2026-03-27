260c41c75450a9834068f7fd60ccb01808365268
Denilson Martinez <aedenilson991@gmail.com>
2026-03-26 23:42:17 -0600

    Cambio en el js de la pagina

--- js/navbar.js
+++ js/navbar.js
@@ -1,23 +1,24 @@
-      const navToggle = document.getElementById("navToggle");
-      const navLinks = document.getElementById("navLinks");
-      const navContacto = document.querySelector('a[href="#contacto"]');
+const navToggle = document.getElementById("navToggle");
+const navLinks = document.getElementById("navLinks");
 
-      navToggle.addEventListener("click", () => {
-        navLinks.classList.toggle("is-open");
-        navToggle.classList.toggle("is-open");
-        document.body.classList.toggle("no-scroll");
-      });
+navToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("is-open");
+  navToggle.classList.toggle("is-open");
+  document.body.classList.toggle("no-scroll");
+});
 
-      navLinks.addEventListener("click", (e) => {
-        if (e.target.closest("a")) {
-          navLinks.classList.remove("is-open");
-          navToggle.classList.remove("is-open");
-          document.body.classList.remove("no-scroll");
-        }
-      });
+navLinks.addEventListener("click", (e) => {
+  if (e.target.closest("a")) {
+    navLinks.classList.remove("is-open");
+    navToggle.classList.remove("is-open");
+    document.body.classList.remove("no-scroll");
+  }
+});
 
-      navContacto.addEventListener("click", () => {
-        navLinks.classList.remove("is-open");
-        navToggle.classList.remove("is-open");
-        document.body.classList.remove("no-scroll");
-      });
+document.addEventListener("click", (e) => {
+  if (!e.target.closest("nav")) {
+    navLinks.classList.remove("is-open");
+    navToggle.classList.remove("is-open");
+    document.body.classList.remove("no-scroll");
+  }
+});
